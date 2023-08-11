ec91802f60b7cfbcbbc141a39dcc8f25c4282978
imvickykumar999 <imvickykumar999@gmail.com>
2023-08-11 21:57:52 +0530

    BCD to 7 Segment Decoder

--- Tutorial/K-Map/tests.py
+++ Tutorial/K-Map/tests.py
@@ -5,13 +5,16 @@ from Kmap import Minterms
 from utils import Term
 
 
-def test_result(
-        str_terms = [6,8,10,11,12,13], 
-        terms_not_care = [9, 14]
-    ):
+def test_result():
     
-    str_terms = [str(format(a,'04b')) for a in str_terms]
-    terms_not_care = [str(format(a,'04b')) for a in terms_not_care]
+    str_terms = ['0000', '0101', '1111', '1010'] 
+    terms_not_care = []
+
+    # str_terms = [6,8,10,11,12,13] 
+    # terms_not_care = [9,14]
+
+    # str_terms = [str(format(a,'04b')) for a in str_terms]
+    # terms_not_care = [str(format(a,'04b')) for a in terms_not_care]
     
     t_minterms = [Term(term) for term in str_terms]
     terms_not_care = [Term(term) for term in terms_not_care]
